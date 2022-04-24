b917ae7913ece868646c91a953b010f74ae48737
kongyi <kongyi123@nate.com>
2022-04-24 14:04:32 +0900

    fix some annotations.

--- app/src/main/java/com/example/recyclerviewpr/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewpr/MainActivity.java
@@ -10,8 +10,6 @@ import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
-    // https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=mail1001&logNo=220682221473&proxyReferer=
     PersonAdapter adapter;
 
     @Override
@@ -19,18 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // (1) 리사이클러뷰의 뷰 찾고
         RecyclerView recyclerView = findViewById(R.id.recyclerView);
 
+        // (2) 리사이클러뷰의 옵션 설정 (모양 및 형태 set)
 //        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
         recyclerView.setLayoutManager(layoutManager); // 리사이클러 뷰가 보여지는 모양을 설정
 
+        // (3) 리사이클러뷰의 adapter 설정 (데이터 set)
         adapter = new PersonAdapter();
         adapter.addItem(new Person("김민수", "010-4005-1234"));
         adapter.addItem(new Person("김띨띨", "010-2039-9934"));
         adapter.addItem(new Person("홍삼삼", "010-2229-1524"));
         recyclerView.setAdapter(adapter);
 
+        // 데이터 원소에 클릭아이템 리스너 설정
         adapter.setOnItemClickListener(new OnPersonItemClickListener() { // 클릭 이벤트 추가
             @Override
             public void onItemClick(PersonAdapter.ViewHolder holder, View view, int position) {
--- app/src/main/java/com/example/recyclerviewpr/PersonAdapter.java
+++ app/src/main/java/com/example/recyclerviewpr/PersonAdapter.java
@@ -35,10 +35,13 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder
     }
 
     @NonNull
-    @Override
+    @Override // 필요한 순간에 보여줄 각 아이템 뷰를 만들어야 하는데, 이게 그 때 호출된다.
+    // 각 아이템의 뷰를 만들어서 리턴하면 각각의 뷰 홀더가 그걸 들고 있는 꼴이다.
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         // ViewHolder라고 하는 걸 생성할 시점에 자동으로 호출됨.
         // 각각의 아이템을위한 뷰홀더임. (뷰 홀더를 만들기만하고 set은 안함, set하는 건 따로 있음)
+        // ViewHolder : 뷰를 갖고 있는 놈
+        // 여기서 원소뷰의 모양을 결정한다.
 
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View itemView = inflater.inflate(R.layout.person_item, parent, false);
@@ -49,8 +52,8 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) { // set만 하는 함수
         // ViewHolder가 계속 새로 만들어지진 않는다. 보이는 부분의 개수만큼만 만들어지고
-        // 나머지는 재사용되는데, 재사용할 때 호출되는 함수임.
-
+        // 나머지는 재사용되는데,  새로 만들든 재사용하든 여튼 그것이 필요할때 바인딩(연결)시켜줄 때 호출되는 놈이다.
+        // position 번째의 홀더에 position 번쨰의 데이터를 받아서 값을 넣어주는 작업을 한다.
         Person item = items.get(position);
         holder.setItem(item);
     }
@@ -60,6 +63,9 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder
         return items.size();
     }
 
+
+    // 이건 리사이클러 뷰의 필수 요소가 아니고
+    // 커스텀 하게 만든 class임.
     static class ViewHolder extends RecyclerView.ViewHolder { // 단지 뷰 하나에 관한 것
         TextView textView;
         TextView textView2;
@@ -70,11 +76,13 @@ public class PersonAdapter extends RecyclerView.Adapter<PersonAdapter.ViewHolder
             textView = itemView.findViewById(R.id.textView);
             textView2 = itemView.findViewById(R.id.textView2);
 
+            // 아이템을 클릭했을 때 발생하는 이벤트 넣기
             itemView.setOnClickListener(new View.OnClickListener() { // 클릭 이벤트 추가
                 @Override
                 public void onClick(View view) {
                     int position = getAdapterPosition();
                     if (listener != null) {
+                        // 바깥(MainActivity)에서 정의한 내용을 수행하도록 만들었음.
                         listener.onItemClick(ViewHolder.this, view, position);
                     }
                 }
